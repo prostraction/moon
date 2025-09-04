d76583804966425df1bf65d6ba4e7a4eafba06c6
prostraction <47314760+prostraction@users.noreply.github.com>
2025-09-04 15:31:48 +0300

    CORS

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -20,10 +20,9 @@ func (s *Server) NewRouter() *fiber.App {
 	})
 
 	app.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-		AllowOrigins:     "*",
-		AllowCredentials: true,
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
 
 	app.Get("/v1/moonTableCurrent", s.moonTableCurrentV1)
