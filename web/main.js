79f11a04b1902b139ffdb3227a51814c2a93dc82
AnnaVas1leva <anna.vasileva@green-api.com>
2025-09-14 21:06:00 +0500

    mobile

--- web/main.js
+++ web/main.js
@@ -1,10 +1,14 @@
 import { createStars } from './render.js';
 import { showMoonDay } from './moon.js';
-import './calendar.js';
+import { initCalendar } from './calendar.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const starsContainer = document.querySelector('.stars');
     createStars(starsContainer, 150);
 
+    // Инициализируем календарь
+    initCalendar();
+    
+    // Показываем данные для текущей даты
     showMoonDay(new Date(), true);
 });
